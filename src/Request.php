416245344319312dbcf22d02a80f5ae0cb2b89df
Sistemas WebDança <48538175+syswd@users.noreply.github.com>
2025-08-22 10:35:54 -0300

    Consertando de vez a cagada do Josantonius

--- src/Request.php
+++ src/Request.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 /**
  * PHP library for handling requests.
  *
@@ -11,361 +8,148 @@ declare(strict_types=1);
  * @link      https://github.com/Josantonius/PHP-Request
  * @since     1.0.0
  */
-
 namespace Josantonius\Request;
 
-use Josantonius\Validate\Validate;
-
 /**
- * Requests handler.
+ * Request handler.
  */
 class Request
 {
     /**
-     * Request params.
+     * Secure access to GET parameters.
      *
-     * @since 2.0.0
+     * @param string $key → request key
      *
-     * @var array
+     * @return mixed|null → value or null
      */
-    protected static $input = [];
-
-    /**
-     * Request params.
-     *
-     * @since 2.0.0
-     *
-     * @var array
-     */
-    protected $params = [];
-
-    /**
-     * Request params.
-     *
-     * @since 2.0.0
-     *
-     * @var array
-     */
-    protected $key = null;
-
-    /**
-     * Get request params.
-     *
-     * @since 2.0.0
-     *
-     * @return anonymous function
-     */
-    public static function input(string $type)
+    public static function get($key = null)
     {
-        $self = self::$input[$type] ?? new self;
-
-        self::$input[$type] = $self;
-
-        switch (strtoupper($type)) {
-            case 'GET':
-            case 'POST':
-                $self->params = filter_input_array(constant('INPUT_' . $type)) ?? [];
-                break;
-            case 'PUT':
-            case 'DELETE':
-                $self->params = self::getParsedInput();
-                break;
+        if (isset($_GET[$key])) {
+            return $_GET[$key];
         }
 
-        return function ($key = null) use ($self) {
-            $self->key = $key;
-
-            return $self;
-        };
+        return (! $key && isset($_GET)) ? $_GET : null;
     }
 
     /**
-     * Data sanitation and return as array.
+     * Secure access to POST parameters.
      *
-     * @since 2.0.0
+     * @param string $key → request key
      *
-     * @param array $filters → associative array with data type for each key
-     * @param mixed $default → default value for non-existent or incorrect keys
+     * @return mixed|null → value or null
      */
-    public function asArray(array $filters = [], $default = null): array
+    public static function post($key = null)
     {
-        $array = Validate::asArray($this->params[$this->key] ?? $this->params, []);
-
-        foreach ($filters as $key => $dataType) {
-            $dataType = 'as' . ucfirst($dataType);
-            $array[$key] = Validate::$dataType($array[$key] ?? null, $default);
+        if (isset($_POST[$key])) {
+            return $_POST[$key];
         }
 
-        return $array;
+        return (! $key && isset($_POST)) ? $_POST : null;
     }
 
     /**
-     * Data sanitation and return as object.
+     * Secure access to FILES parameters.
      *
-     * @since 2.0.0
+     * @param string $key → request key
      *
-     * @param array $filters → object with data type for each key
-     * @param mixed $default → default value for non-existent or incorrect keys
+     * @return mixed|null → value or null
      */
-    public function asObject(array $filters = [], $default = null): \stdClass
+    public static function files($key = null)
     {
-        $object = Validate::asObject($this->params[$this->key] ?? $this->params, (object) []);
-
-        foreach ($filters as $key => $dataType) {
-            $dataType = 'as' . ucfirst($dataType);
-            $object->{$key} = Validate::$dataType($object->{$key} ?? null, $default);
+        if (isset($_FILES[$key])) {
+            return $_FILES[$key];
         }
 
-        return $object;
+        return (! $key && isset($_FILES)) ? $_FILES : null;
     }
 
     /**
-     * Data sanitation and return as JSON.
+     * Secure access to PUT parameters.
      *
-     * @since 2.0.0
+     * @param string $key → request key
      *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
+     * @return mixed|null → value or null
      */
-    public function asJson($default = null)
+    public static function put($key = null)
     {
-        return Validate::asJson($this->params[$this->key] ?? $this->params, $default);
+        parse_str(file_get_contents('php://input'), $_PUT);
+
+        if (isset($_PUT[$key])) {
+            return $_PUT[$key];
+        }
+
+        return (! $key && isset($_PUT)) ? $_PUT : null;
     }
 
     /**
-     * Data sanitation and return as string.
+     * Secure access to DELETE parameters.
      *
-     * @since 2.0.0
+     * @param string $key → request key
      *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
+     * @return mixed|null → value or null
      */
-    public function asString($default = null)
+    public static function del($key = null)
     {
-        return Validate::asString($this->params[$this->key] ?? null, $default);
-    }
+        parse_str(file_get_contents('php://input'), $_DEL);
+        
+        if (isset($_DEL[$key])) {
+            return $_DEL[$key];
+        }
 
-    /**
-     * Data sanitation and return as integer number.
-     *
-     * @since 2.0.0
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asInteger($default = null)
-    {
-        return Validate::asInteger($this->params[$this->key] ?? null, $default);
-    }
-
-    /**
-     * Data sanitation and return as float number.
-     *
-     * @since 2.0.0
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asFloat($default = null)
-    {
-        return Validate::asFloat($this->params[$this->key] ?? null, $default);
-    }
-
-    /**
-     * Data sanitation and return as boolean.
-     *
-     * @since 2.0.0
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asBoolean($default = null)
-    {
-        return Validate::asBoolean($this->params[$this->key] ?? [], $default);
-    }
-
-    /**
-     * Data sanitation and return as IP.
-     *
-     * @since 2.0.0
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asIp($default = null)
-    {
-        return Validate::asIp($this->params[$this->key] ?? null, $default);
-    }
-
-    /**
-     * Data sanitation and return as URL.
-     *
-     * @since 2.0.0
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asUrl($default = null)
-    {
-        return Validate::asUrl($this->params[$this->key] ?? null, $default);
-    }
-
-    /**
-     * Data sanitation and return as email.
-     *
-     * @since 2.0.0
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asEmail($default = null)
-    {
-        return Validate::asEmail($this->params[$this->key] ?? null, $default);
-    }
-
-    /**
-     * Data sanitation and return as email.
-     *
-     * @since 2.0.1
-     *
-     * @param mixed $default → default value
-     *
-     * @return mixed → value, null or customized return value
-     */
-    public function asDatetime($default = null)
-    {
-        return Validate::asDatetime($this->params[$this->key] ?? null, $default);
+        return (! $key && isset($_DEL)) ? $_DEL : null;
     }
 
     /**
      * Check if it is a GET request.
+     *
+     * @return bool
      */
-    public static function isGet(): bool
+    public static function isGet()
     {
         return $_SERVER['REQUEST_METHOD'] === 'GET';
     }
 
     /**
      * Check if it is a POST request.
+     *
+     * @return bool
      */
-    public static function isPost(): bool
+    public static function isPost()
     {
         return $_SERVER['REQUEST_METHOD'] === 'POST';
     }
 
     /**
      * Check if it is a PUT request.
+     *
+     * @return bool
      */
-    public static function isPut(): bool
+    public static function isPut()
     {
         return $_SERVER['REQUEST_METHOD'] === 'PUT';
     }
 
     /**
      * Check if it is a DELETE request.
+     *
+     * @return bool
      */
-    public static function isDelete(): bool
+    public static function isDelete()
     {
         return $_SERVER['REQUEST_METHOD'] === 'DELETE';
     }
 
     /**
-     * Get parsed input.
+     * Check if it is a AJAX request.
      *
-     * @return mixed|null → value or null
+     * @return bool
      */
-    private static function getParsedInput()
+    public static function isAjax()
     {
-        $input = file_get_contents('php://input') ?: null;
-
-        switch (self::getContentType()) {
-            case 'application/atom+xml':
-                $input = new \SimpleXmlElement($input);
-                break;
-            case 'text/html':
-            case 'text/plain':
-            case 'application/json':
-            case 'application/javascript':
-                $input = json_decode($input, true);
-                break;
-            case 'multipart/form-data':
-            case 'application/x-www-form-urlencoded':
-                $data = [];
-                self::parseRaw($input, $data);
-                $input = $data;
-                break;
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+            return strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
         }
 
-        return $input ?? [];
-    }
-
-    /**
-     * Get request content type.
-     *
-     * @since 2.0.0
-     */
-    private static function getContentType(): string
-    {
-        $contentType = $_SERVER['HTTP_CONTENT_TYPE'] ?? '';
-
-        return explode(';', $contentType)[0];
-    }
-
-    /**
-     * Parse raw http request to array.
-     *
-     * Based on parse_raw_http_request function of @Christof.
-     *
-     * @link https://stackoverflow.com/a/5488449
-     *
-     * @since 2.0.0
-     */
-    private static function parseRaw(string $input, array &$data = [])
-    {
-        preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
-
-        $blocks = preg_split('/-+' . ($matches[1] ?? '') . '/', $input);
-
-        array_pop($blocks);
-
-        foreach ($blocks as $id => $block) {
-            if (empty($block)) {
-                continue;
-            }
-            if (strpos($block, 'application/octet-stream') !== false) {
-                preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
-            } else {
-                preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
-            }
-            $data[$matches[1]] = $matches[2];
-        }
-    }
-
-    public function get()
-    {
-        return static::input('get');
-    }
-
-    public function post()
-    {
-        return static::input('post');
-    }
-
-    public function delete()
-    {
-        return static::input('delete');
-    }
-
-    public function put()
-    {
-        return static::input('put');
+        return false;
     }
 }
